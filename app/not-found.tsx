1b77b7d4a51337f0fa4173dc6c45574d3f8bd05d
serhatunver <unverserhat2@gmail.com>
2025-12-28 23:39:49 +0100

    feat: add empty and integrate into not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,22 @@
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyTitle,
+} from '@/components/ui/empty';
+
 export default function NotFound() {
-  return <h1>404</h1>;
+  return (
+    <Empty className="py-20">
+      <EmptyContent>
+        <EmptyHeader>
+          <EmptyTitle>404 - Page Not Found</EmptyTitle>
+          <EmptyDescription>
+            Oops! The page you are looking for does not exist.
+          </EmptyDescription>
+        </EmptyHeader>
+      </EmptyContent>
+    </Empty>
+  );
 }
