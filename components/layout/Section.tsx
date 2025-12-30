2bb190b189b3a13ce74bee2028650621eff2bf70
serhatunver <unverserhat2@gmail.com>
2025-12-30 01:43:18 +0100

    feat: add description to Section component

--- components/layout/Section.tsx
+++ components/layout/Section.tsx
@@ -4,24 +4,31 @@ import { ArrowRight } from 'lucide-react';
 
 export function Section({
   title,
+  description,
   seeAllLink,
   children,
 }: {
   title: string;
+  description?: string;
   seeAllLink?: string;
   children: React.ReactNode;
 }) {
   return (
-    <section className="py-4">
-      <div className="flex w-full items-center justify-between mb-4">
-        <h2 className="text-lg font-medium">{title}</h2>
-        {seeAllLink && (
-          <Button asChild variant="link" size="sm">
-            <Link href={seeAllLink}>
-              See all
-              <ArrowRight className="size-4" />
-            </Link>
-          </Button>
+    <section className="py-8">
+      <div className="mb-6">
+        <div className="flex w-full items-center justify-between">
+          <h2 className="text-lg font-medium">{title}</h2>
+          {seeAllLink && (
+            <Button asChild variant="link" size="sm">
+              <Link href={seeAllLink}>
+                See all
+                <ArrowRight className="size-4" />
+              </Link>
+            </Button>
+          )}
+        </div>
+        {description && (
+          <p className="text-sm text-muted-foreground">{description}</p>
         )}
       </div>
       <div>{children}</div>
