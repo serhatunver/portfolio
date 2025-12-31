78fd1659117fa9b51f4fb4469338eda4fd402064
serhatunver <unverserhat2@gmail.com>
2025-12-31 20:14:01 +0100

    feat: refactor Section and add new props

--- components/layout/Section.tsx
+++ components/layout/Section.tsx
@@ -1,36 +1,72 @@
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
-import { ArrowRight } from 'lucide-react';
+import {
+  ChevronsRight,
+  ChevronRight,
+  ExternalLink,
+  ArrowRight,
+} from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+type SectionProps = {
+  title?: string;
+  description?: string;
+  seeAllLink?: string;
+  ariaLabel?: string;
+  paddingNone?: boolean;
+  className?: string;
+  children: React.ReactNode;
+};
 
 export function Section({
   title,
   description,
   seeAllLink,
+  ariaLabel,
+  paddingNone = false,
+  className,
   children,
-}: {
-  title: string;
-  description?: string;
-  seeAllLink?: string;
-  children: React.ReactNode;
-}) {
+}: SectionProps) {
+  const hasHeader = title || description || seeAllLink;
+
   return (
-    <section className="py-8">
-      <div className="mb-6">
-        <div className="flex w-full items-center justify-between">
-          <h2 className="text-lg font-medium">{title}</h2>
-          {seeAllLink && (
-            <Button asChild variant="link" size="sm">
-              <Link href={seeAllLink}>
-                See all
-                <ArrowRight className="size-4" />
-              </Link>
-            </Button>
+    <section
+      className={cn(paddingNone ? 'py-0' : 'py-4 sm:py-6 md:py-8', className)}
+      aria-label={ariaLabel ?? title}
+    >
+      {hasHeader && (
+        <header className="mb-5 space-y-1">
+          {(title || seeAllLink) && (
+            <div className="flex items-center justify-between">
+              {title && (
+                <h2 className="flex items-center text-xl font-medium tracking-tight">
+                  <ChevronsRight size={20} />
+                  <span>{title}</span>
+                </h2>
+              )}
+
+              {seeAllLink && (
+                <Button
+                  asChild
+                  variant="link"
+                  size="sm"
+                  className="has-[>svg]:px-0 px-0"
+                >
+                  <Link href={seeAllLink} className="leading-0">
+                    See all
+                    <ArrowRight className="size-4" />
+                  </Link>
+                </Button>
+              )}
+            </div>
           )}
-        </div>
-        {description && (
-          <p className="text-sm text-muted-foreground">{description}</p>
-        )}
-      </div>
+
+          {description && (
+            <p className="text-sm text-muted-foreground">{description}</p>
+          )}
+        </header>
+      )}
+
       <div>{children}</div>
     </section>
   );
