e65e2a72c0d351d492664ef2895e2f7564ea99cf
serhatunver <unverserhat2@gmail.com>
2026-01-01 22:55:11 +0100

    fix: fix overlapping issue in EducationCard

--- components/common/EducationCard.tsx
+++ components/common/EducationCard.tsx
@@ -15,7 +15,7 @@ import { formatDuration } from '@/lib/utils/date';
 
 export function EducationCard({ education }: { education: Education }) {
   return (
-    <Card className="w-full border-none shadow-none rounded-2xl bg-background gap-4 py-4">
+    <Card className="w-full border-none shadow-none bg-background gap-4 py-4">
       <CardHeader className="flex items-center gap-4 px-0">
         <Avatar className="size-12 border-2">
           <AvatarImage
@@ -32,36 +32,21 @@ export function EducationCard({ education }: { education: Education }) {
         </Avatar>
         <CardTitle className="flex flex-col text-base tracking-tight">
           <span className="text-base">{education.institution}</span>
-          <span className=" text-sm text-muted-foreground font-normal">
+          <CardDescription className="font-normal">
             {education.degree} &bull; {education.department}
-          </span>
-          <CardDescription>
-            {/* {education.degree} &bull; {education.department} */}
           </CardDescription>
         </CardTitle>
       </CardHeader>
-      <CardContent className="flex h-6 flex-wrap gap-3 px-0">
-        <Badge
-          variant="outline"
-          // className="bg-[#FFFC00]/15 border-[#FFFC00]/90 leading-none"
-          className="bg-muted"
-        >
+      <CardContent className="flex flex-wrap gap-2 px-0">
+        <Badge variant="outline" className="bg-muted">
           <GraduationCap />
           <span>GPA: {education.gpa}</span>
         </Badge>
-        <Badge
-          variant="outline"
-          // className="bg-[#00DC82]/10 border-[#00DC82]/50 leading-none"
-          className="bg-muted"
-        >
+        <Badge variant="outline" className="bg-muted">
           <Calendar />
           <span>{formatDuration(education.startDate, education.endDate)}</span>
         </Badge>
-        <Badge
-          variant="outline"
-          // className="bg-[#00DC82]/10 border-[#00DC82]/50"
-          className="bg-muted"
-        >
+        <Badge variant="outline" className="bg-muted">
           <MapPin />
           <span>{education.location}</span>
         </Badge>
