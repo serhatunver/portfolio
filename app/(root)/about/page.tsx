d0d8a5a14e17ea15ab58a508030be3637a369c01
serhatunver <unverserhat2@gmail.com>
2026-01-07 00:05:16 +0100

    refactor: reorganize home and about pages content

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -2,16 +2,14 @@ import { AboutSection } from '@/components/sections/AboutSection';
 import { ExperienceSection } from '@/components/sections/ExperienceSection';
 import { EducationSection } from '@/components/sections/EducationSection';
 import { TechStackSection } from '@/components/sections/TechStackSection';
-import { ProjectsSection } from '@/components/sections/ProjectsSection';
 
 export default function About() {
   return (
     <>
       <AboutSection />
+      <TechStackSection />
       <EducationSection />
       <ExperienceSection />
-      <TechStackSection />
-      <ProjectsSection limit={3} seeAllLink="/projects" />
     </>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,5 @@
 import { HeroSection } from '@/components/sections/HeroSection';
 import { ExperienceSection } from '@/components/sections/ExperienceSection';
-import { EducationSection } from '@/components/sections/EducationSection';
 import { ProjectsSection } from '@/components/sections/ProjectsSection';
 
 export default function Home() {
@@ -8,7 +7,6 @@ export default function Home() {
     <>
       <HeroSection />
       <ExperienceSection />
-      <EducationSection />
       <ProjectsSection limit={2} seeAllLink="/projects" />
     </>
   );
